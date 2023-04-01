5ae944b6570a55012c713a93bfe9f88a50fa574e
Elena-Baixy <elaina3232@sjtu.edu.cn>
2023-03-31 23:13:03 -0400

    count

--- inverted_index/map0.py
+++ inverted_index/map0.py
@@ -1,1 +1,18 @@
+#!/usr/bin/env python3
 """Map 0."""
+import csv
+import sys
+
+csv.field_size_limit(sys.maxsize)
+
+def count():
+    '''Job 0'''
+    with open('inverted_index/input/input.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        rows = list(csv_reader)
+        length = len(rows)
+        print(length)
+    with open('inverted_index/output0/total_document_count.txt', 'w') as output_file:
+        output_file.write(str(length))
+
+count()
