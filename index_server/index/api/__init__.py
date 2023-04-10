1a1f271f71ab8d9af17d4380fb5ccd297f156e95
Zhihao Yang <zhihaoy@umich.edu>
2023-04-09 22:32:13 -0400

    index_server/init added

--- index_server/index/api/__init__.py
+++ index_server/index/api/__init__.py
@@ -3,7 +3,7 @@
 # import index.api  # noqa: E402  pylint: disable=wrong-import-position
 
 # Load inverted index, stopwords, and pagerank into memory
-from index.api.main import get_service
+from index.api.main import load_index
 
 
 
